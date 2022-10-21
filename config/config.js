148872da537268ac1203a1e7b854a3215a4dde63
Andrea Cavalcanti <67080323+Andreacnas@users.noreply.github.com>
2022-10-21 18:44:22 -0400

    Initial code Week 6

---
+++ config/config.js
@@ -0,0 +1,3 @@
+export const Secret = "MySecret";
+// export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://andreacnas:tapioca123@cluster0.ibkzrfp.mongodb.net/?retryWrites=true&w=majority";
